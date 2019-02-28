b054c360f73fdc6719133b7b4af61c547bbd24f6
Yevgeny Antonov <yevgeny1978@mail.ru>
2019-02-28 11:16:25 +0700

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -9,14 +9,12 @@ extern const int FIELD_HEIGHT;
 extern const int BLOCK_WIDTH;
 extern const int BLOCK_HEIGHT;
 
-
 class Ball
 {
 public:
 	Ball();
 	void update(sf::Time elapsed);
 	void draw(sf::RenderWindow& window);
-	~Ball();
 private:
 	float x, y, dx, dy;
 	sf::RectangleShape shape;
